d14412537b53781b2b98d512139365e1caaf33d6
adilson.gditecnologia@gmail.com <adilson.gditecnologia@gmail.com>
2023-05-09 16:58:48 -0300

    ajuste

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -44,7 +44,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
           title: Text(
-            'Page Title',
+            'Page Title Teste',
             style: FlutterFlowTheme.of(context).headlineMedium.override(
                   fontFamily: 'Poppins',
                   color: Colors.white,
